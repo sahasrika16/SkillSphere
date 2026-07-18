c035bf914d64c209d2d3260ad82226e81379c0f3
Sahasrika <sahasrika04@gmail.com>
2026-07-18 20:51:48 +0530

    Fix API base URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skillsphere-backend-72pj.onrender.com",
+  baseURL: "https://skillsphere-backend-72pj.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
