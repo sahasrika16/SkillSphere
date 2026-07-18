a8c75b042b0fc2230972995502b7636747a96f3c
Sahasrika <sahasrika04@gmail.com>
2026-07-18 19:36:30 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ Socket.IO
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
   }
 });
@@ -104,7 +104,7 @@ Middlewares
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
   })
 );
